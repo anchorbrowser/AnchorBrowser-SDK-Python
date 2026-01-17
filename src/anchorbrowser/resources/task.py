b4d3439b81b22881604d58c323137fded282d101
Gil Dankner <gil@anchorbrowser.io>
2026-01-18 01:46:22 +0200

    lint fix

--- src/anchorbrowser/resources/task.py
+++ src/anchorbrowser/resources/task.py
@@ -16,7 +16,7 @@ from .._response import (
     to_raw_response_wrapper,
     to_streamed_response_wrapper,
     async_to_raw_response_wrapper,
-    async_to_streamed_response_wrapper
+    async_to_streamed_response_wrapper,
 )
 from .._base_client import make_request_options
 from ..types.task_run_response import TaskRunResponse
